08266b866707b726e2a6bb68798c80d5be3170d1
Phuc X. Doan <phucxdoan@gmail.com>
2026-09-14 13:12:40 +0000

    Using `linkFarm`.

--- default.nix
+++ default.nix
@@ -7,67 +7,74 @@ let
     system = "x86_64-linux";
   };
 
-in {
+in rec {
 
 
 
-    pdftotext_asan = pkgs.stdenv.mkDerivation {
-
-      name = "xpdf-3.02-pdftotext-asan";
-      src  = pkgs.fetchurl {
-        url    = "https://dl.xpdfreader.com/old/xpdf-3.02.tar.gz";
-        sha256 = "szp9VvRUwzGuUJlvmJ6GyRZuV6+Xt03ijN3z1RrBHwA=";
-      };
+  default = pkgs.linkFarm "builds" [
+    { name = "pdftotext_asan";       path = pdftotext_asan;       }
+    { name = "pdftotext_asan_ubsan"; path = pdftotext_asan_ubsan; }
+  ];
 
 
 
-      # Environmental variables.
+  pdftotext_asan = pkgs.stdenv.mkDerivation {
 
-      CC       = "${pkgs.aflplusplus}/bin/afl-clang-fast";
-      CXX      = "${pkgs.aflplusplus}/bin/afl-clang-fast++";
-      CFLAGS   = "-w -Og -fsanitize=address";
-      CXXFLAGS = "-w -Og -fsanitize=address";
-
-
-
-      # The only program we care about is `pdftotext`.
-
-      installPhase = ''
-        mkdir -p $out/
-        cp ./xpdf/pdftotext $out/
-      '';
-
+    name = "pdftotext_asan";
+    src  = pkgs.fetchurl {
+      url    = "https://dl.xpdfreader.com/old/xpdf-3.02.tar.gz";
+      sha256 = "szp9VvRUwzGuUJlvmJ6GyRZuV6+Xt03ijN3z1RrBHwA=";
     };
 
 
 
-    pdftotext_asan_ubsan = pkgs.stdenv.mkDerivation {
+    # Environmental variables.
 
-      name = "xpdf-3.02-pdftotext-asan-ubsan";
-      src  = pkgs.fetchurl {
-        url    = "https://dl.xpdfreader.com/old/xpdf-3.02.tar.gz";
-        sha256 = "szp9VvRUwzGuUJlvmJ6GyRZuV6+Xt03ijN3z1RrBHwA=";
-      };
-
-
-
-      # Environmental variables.
-
-      CC       = "${pkgs.aflplusplus}/bin/afl-clang-fast";
-      CXX      = "${pkgs.aflplusplus}/bin/afl-clang-fast++";
-      CFLAGS   = "-w -Og -fsanitize=address,undefined";
-      CXXFLAGS = "-w -Og -fsanitize=address,undefined";
+    CC       = "${pkgs.aflplusplus}/bin/afl-clang-fast";
+    CXX      = "${pkgs.aflplusplus}/bin/afl-clang-fast++";
+    CFLAGS   = "-w -Og -fsanitize=address";
+    CXXFLAGS = "-w -Og -fsanitize=address";
 
 
 
-      # The only program we care about is `pdftotext`.
+    # The only program we care about is `pdftotext`.
 
-      installPhase = ''
-        mkdir -p $out/
-        cp ./xpdf/pdftotext $out/
-      '';
+    installPhase = ''
+      mkdir -p $out/
+      cp ./xpdf/pdftotext $out/
+    '';
 
+  };
+
+
+
+  pdftotext_asan_ubsan = pkgs.stdenv.mkDerivation {
+
+    name = "pdftotext_asan_ubsan";
+    src  = pkgs.fetchurl {
+      url    = "https://dl.xpdfreader.com/old/xpdf-3.02.tar.gz";
+      sha256 = "szp9VvRUwzGuUJlvmJ6GyRZuV6+Xt03ijN3z1RrBHwA=";
     };
+
+
+
+    # Environmental variables.
+
+    CC       = "${pkgs.aflplusplus}/bin/afl-clang-fast";
+    CXX      = "${pkgs.aflplusplus}/bin/afl-clang-fast++";
+    CFLAGS   = "-w -Og -fsanitize=address,undefined";
+    CXXFLAGS = "-w -Og -fsanitize=address,undefined";
+
+
+
+    # The only program we care about is `pdftotext`.
+
+    installPhase = ''
+      mkdir -p $out/
+      cp ./xpdf/pdftotext $out/
+    '';
+
+  };
 
 
 
