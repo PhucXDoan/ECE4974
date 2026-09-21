d8d7579c595c9211bf71a5079d05adba59eeedde
Phuc X. Doan <phucxdoan@gmail.com>
2026-09-21 15:58:32 +0000

    Using short URL for Nixpkgs.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
 
   pkgs = import (fetchTarball {
-    url    = "https://github.com/NixOS/nixpkgs/archive/8c50a710ddca43d7a530fb805ad55bde8d0141c5.tar.gz";
+    url    = "https://github.com/NixOS/nixpkgs/archive/refs/tags/26.05.tar.gz";
     sha256 = "0am8xx09fx5yf2p0wb001v0jx1g5hrfb76h4r37xph378jgk7pcr";
   }) {
     system = "x86_64-linux";
