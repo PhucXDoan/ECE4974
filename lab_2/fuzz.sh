b74be54d288f6aae07a0444b1dd66bac8202c825
Phuc X. Doan <phucxdoan@gmail.com>
2026-09-22 23:37:47 +0000

    More fuzzing results for `miniz`.

--- lab_2/fuzz.sh
+++ lab_2/fuzz.sh
@@ -1,6 +1,7 @@
-afl-fuzz                \
-    -i ./lab_2/examples \
-    -o ./lab_2/out      \
-    -s 123              \
-    -m none             \
+afl-fuzz                  \
+    -i ./lab_2/examples   \
+    -o ./lab_2/out        \
+    -s 123                \
+    -m none               \
+    -x ./lab_2/dictionary \
     -- ./result/miniz_harness/miniz_harness @@
